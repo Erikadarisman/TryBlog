d5401d8baf0893ff5eff119d7ff6905072aaedca
Erik <erikadarisman@gmail.com>
2020-02-09 21:57:49 +0700

    updated

--- app/src/main/java/com/example/trynoteapp/MainActivity.kt
+++ app/src/main/java/com/example/trynoteapp/MainActivity.kt
@@ -5,14 +5,35 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var blogAdapter: TestRecylerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initRecylerView()
+        addDataset()
 
 
+    }
+
+    private fun addDataset() {
+        val data = DataSource.createDataSet()
+        blogAdapter.submitList(data)
+    }
+
+    private fun initRecylerView() {
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            val topSpacingDecoration = TopSpacingitemDecoration(30)
+            addItemDecoration(topSpacingDecoration)
+            blogAdapter = TestRecylerAdapter()
+            adapter = blogAdapter
+        }
     }
 
 
--- app/src/main/java/com/example/trynoteapp/models/TestPost.kt
+++ app/src/main/java/com/example/trynoteapp/models/TestPost.kt
@@ -2,7 +2,9 @@ package com.example.trynoteapp.models
 
 data class TestPost(
     var title: String,
-    var desc: String
+    var body: String,
+    var image: String,
+    var user: String
 ){
 
 }
